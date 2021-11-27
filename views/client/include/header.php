4adb4d5950c933c5401c7ed68e326ae73b0770a8
trungdang <dientrung.py3@gmail.com>
2021-11-28 01:35:46 +0700

    Add logo

--- views/client/include/header.php
+++ views/client/include/header.php
@@ -13,7 +13,7 @@
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark border-bottom sticky-top">
     <a class="navbar-brand" href="../home/index.php">
-        Bookstore
+        <img src="../../../assets/images/home/logo.png" alt="BookStore" class="" with='35px' height='35px'>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
